0947c1d59eb5c21248fdfcfb134ad3fbb0b17a81
b6320502452 <pavich.s@ku.th>
2021-02-15 17:21:40 +0700

    Update lab3.1.c

--- lab3.1.c
+++ lab3.1.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int a[5][4],i,j,sum[5][1];
+    int a[5][4],i,j,sum[5];
     for(i=0;i<5;i++)
     {
-        sum[i][0]=0;
+        sum[i]=0;
         for(j=0;j<4;j++)
         {
             scanf("%d",&a[i][j]);
-            sum[i][0]+=a[i][j];
+            sum[i]+=a[i][j];
         }
     }
-    int max=sum[0][0];
+    int max=sum[0];
     int pos=0;
     for(i=1;i<5;i++)
     {
-        if(sum[i][0]>max)
+        if(sum[i]>max)
+        {
+            max=sum[i];
             pos=i;
+        }
     }
-    printf("%d %d",pos+1,sum[pos][0]);
+    printf("%d %d",pos+1,sum[pos]);
 }
