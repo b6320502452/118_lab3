3f416ef23bdadcb57de9b963e82042cedb68ea3c
b6320502452 <pavich.s@ku.th>
2021-02-15 20:11:45 +0700

    Update lab3.6.c

--- lab3.6.c
+++ lab3.6.c
@@ -2,9 +2,9 @@
 int main()
 {
     long long n,i;
-    int pri,di=0;
+    int pri;
     scanf("%lld",&n);
-    /*for(n;n>=0;n--)
+    for(n;n>=0;n--)
     {
         pri=1;
         for(i=2;i<n;i++)
@@ -16,14 +16,34 @@ int main()
             }
         }
         if(pri==1)
-        {*/
-            while(n!=0)
+        {
+            int di=0,k,pa=1;
+            long long cn=n,temn;
+            while(cn!=0)
             {
-                n/=10;
+                cn/=10;
                 di++;
             }
-            printf("%d",di);
-        /*}
+            cn=n;
+            int a[di];
+            for(k=0;k<di;k++)
+            {
+                a[k]=cn%10;
+                cn/=10;
+            }
+            int cdi=di;
+            for(k=0;k<di;k++)
+            {
+                temn+=a[di-k-1]*cdi;
+                cdi/10;
+            }
+            if(temn==n)
+            {
+                printf("%d",n);
+                break;
+            }
 
-    }*/
+        }
+
+    }
 }
