362e2288052b685fffda7e8add42560d6441d4da
b6320502452 <pavich.s@ku.th>
2021-02-15 18:21:23 +0700

    Update lab3.4.c

--- lab3.4.c
+++ lab3.4.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
+    long long n;
+    scanf("%lld",&n);
     for(n;n>0;n--)
     {
         if((n%2!=0&&n%3!=0&&n%5!=0&&n%7!=0)||n==7||n==5||n==3||n==2)
         {
-            printf("%d",n);
+            printf("%lld",n);
             break;
         }
     }
